33a9f2c11b975d5f4f7631af65011e1ed1a0c5f6
jason <jason@f0fbaf97-c700-0410-a5eb-8ea856f8537e>
2005-09-18 02:15:11 +0000

    yay, events on the homepage
    git-svn-id: http://svn.pdxruby.org/repos/www/trunk@61 f0fbaf97-c700-0410-a5eb-8ea856f8537e

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,6 @@
 class IndexController < ApplicationController
   def index
     @upcoming_events = Event.find :all, :conditions => "starts_at > '#{Time.now}' AND status = '#{Event::EVENT_STATUS[:active]}'" 
+    @recent_events   = Event.find :all, :conditions => "starts_at < '#{Time.now}' AND status = '#{Event::EVENT_STATUS[:active]}'"
   end
 end
