0997e6fcf331623eeb2fcdca0c3af42d9983d8b3
careo <careo@f0fbaf97-c700-0410-a5eb-8ea856f8537e>
2005-09-18 00:30:57 +0000

    Added validation to location model.
    git-svn-id: http://svn.pdxruby.org/repos/www/trunk@46 f0fbaf97-c700-0410-a5eb-8ea856f8537e

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,10 @@
 class Location < ActiveRecord::Base
   has_many :events
 
+  validates_presence_of :name
+  validates_presence_of :address
+
+  validates_uniqueness_of :name
+  validates_uniqueness_of :address
+  
 end
