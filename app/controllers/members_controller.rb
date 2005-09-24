d41b47989327bfafd3442f6bba2852afa449baf4
caleb <caleb@f0fbaf97-c700-0410-a5eb-8ea856f8537e>
2005-09-24 05:45:39 +0000

    added support for uploading member mugshots. to do dynamic resizing, the RMagick gem is needed (http://rmagick.rubyforge.org/). RMagick requires ImageMagick >6 to function.
    some basic validation is done on the uploaded image:
    * content type must be image/jpeg
    * must be less than 100kb
    
    images are uploaded to public/images/members, which must be writable by rails.
    
    
    git-svn-id: http://svn.pdxruby.org/repos/www/trunk@102 f0fbaf97-c700-0410-a5eb-8ea856f8537e

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,6 +4,8 @@ class MembersController < ApplicationController
   before_filter :member_is_this_member!, :only => [ :edit, :update ]
   before_filter :member_exists!, :only => [ :show ]
   
+  require 'RMagick'
+  
   def index
     redirect_to :action => 'login'
   end
@@ -29,6 +31,13 @@ class MembersController < ApplicationController
       return
     end
     if @member.save
+      begin
+        save_mugshot params[:image_file]
+      rescue StandardError => message
+        flash[:notice] = message
+        redirect_to :action => 'edit', :id => @member
+	return
+      end
       flash[:notice] = 'Member was successfully created.'
       session[:member] = @member
       redirect_to :action => 'show', :id => @member.id
@@ -49,6 +58,13 @@ class MembersController < ApplicationController
       return
     end
     if @member.update_attributes(params[:member])
+      begin
+        save_mugshot params[:image_file]
+      rescue StandardError => message
+        flash[:notice] = message
+        redirect_to :action => 'edit', :id => @member
+	return
+      end
       flash[:notice] = 'Member was successfully updated.'
       redirect_to :action => 'show', :id => @member
     else
@@ -106,6 +122,38 @@ class MembersController < ApplicationController
       flash[:notice] = "No Peeking."
       redirect_to :action => 'list'
     end
+  end
+  
+  def save_mugshot(image_file)
+    if (image_file.nil?) or (image_file.size == 0)
+      return
+    end
+    
+    filename = "images/members/" + @member.id.to_s + ".jpg"
+    # make sure filetype is okay
+    if image_file.content_type.scan(/image\/jpeg/).empty?
+      raise "Mugshot must be of type image/jpeg. (not " + image_file.content_type + ")"
+      return
+    end
+    
+    # make sure it is less than 100k
+    if image_file.size > 100000
+      raise "Mugshot cannot be larger than 100k bytes."
+      return
+    end
+    
+    # if the file exists already, delete it
+    if File.exist?(filename)
+      File.unlink(filename)
+    end
+    file = File.new(filename,File::CREAT|File::WRONLY)
+    file.write(params[:image_file].read)
+    file.close
+    
+    # you can comment out these three lines if you don't want to do dynamic resizing
+    im = Magick::ImageList.new filename
+    im.change_geometry!("64x64"){ |cols,rows,img| img.resize!(cols,rows) }
+    im.write filename
   end
 
 end
