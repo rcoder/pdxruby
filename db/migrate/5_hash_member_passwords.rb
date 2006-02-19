86385aa7bd88176556c781b2d14ba480b4e5a2a5
ben <ben@f0fbaf97-c700-0410-a5eb-8ea856f8537e>
2006-02-19 21:02:52 +0000

    Remove question from Eric, answered on mailing list
    git-svn-id: http://svn.pdxruby.org/repos/www/trunk@179 f0fbaf97-c700-0410-a5eb-8ea856f8537e

--- db/migrate/5_hash_member_passwords.rb
+++ db/migrate/5_hash_member_passwords.rb
@@ -1,7 +1,5 @@
 require 'digest/sha1'
 
-# XXX elw:  does this work if the existing passwords are already md5?
-
 class HashMemberPasswords < ActiveRecord::Migration
   def self.up
     print "Hashing member passwords... "
