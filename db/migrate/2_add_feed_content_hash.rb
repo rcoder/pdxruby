f442f86b8657cac497f1185201a3ce591b7c45bb
keegan <keegan@f0fbaf97-c700-0410-a5eb-8ea856f8537e>
2006-04-12 03:57:34 +0000

    Changed migration to use an ActiveRecord method rather than SQL.
    git-svn-id: http://svn.pdxruby.org/repos/www/trunk@194 f0fbaf97-c700-0410-a5eb-8ea856f8537e

--- db/migrate/2_add_feed_content_hash.rb
+++ db/migrate/2_add_feed_content_hash.rb
@@ -1,7 +1,7 @@
 class AddFeedContentHash < ActiveRecord::Migration
   def self.up
     puts "Adding new char(32) column articles.content_hash"
-    execute "ALTER TABLE articles ADD COLUMN content_hash CHARACTER(32)" 
+    add_column :articles, :content_hash, :string, :limit => 32
   end
 
   def self.down
