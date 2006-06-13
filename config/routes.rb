f5d6a9e877abad2efcedb99332ab30daad07adfd
keegan <keegan@f0fbaf97-c700-0410-a5eb-8ea856f8537e>
2006-06-13 12:32:29 +0000

    Cleaned up some comments and increased the priority of the default route.
    git-svn-id: http://svn.pdxruby.org/repos/www/trunk@223 f0fbaf97-c700-0410-a5eb-8ea856f8537e

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   # Add your own custom routes here.
   # The priority is based upon order of creation: first created -> highest priority.
   
-  # Here's a sample route:
-  # map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # You can have the root of your site routed by hooking up '' 
-  # -- just remember to delete public/index.html.
-  # map.connect '', :controller => "welcome"
+  map.connect '', :controller => "index", :action => "index"
 
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
@@ -16,6 +10,4 @@ ActionController::Routing::Routes.draw do |map|
 
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id'
-  map.connect '', :controller   =>  "index",
-                 :action       =>  "index"
 end
