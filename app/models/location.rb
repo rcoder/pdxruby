422faba7700d3c1b479b5836f59373d89050b356
keegan <keegan@f0fbaf97-c700-0410-a5eb-8ea856f8537e>
2006-06-17 12:46:52 +0000

    Allow nil in addition to empty strings for non-required fields.
    git-svn-id: http://svn.pdxruby.org/repos/www/trunk@233 f0fbaf97-c700-0410-a5eb-8ea856f8537e

--- app/models/location.rb
+++ app/models/location.rb
@@ -8,14 +8,12 @@ class Location < ActiveRecord::Base
   validates_uniqueness_of :address
 
   validates_length_of :name, :maximum => 64
-  validates_length_of :homepage, :maximum => 256
+  validates_length_of :homepage, :maximum => 256, :allow_nil => true
 
   validates_each :homepage do |rec, attr|
     re = Regexp.new(RE::URL)
-    unless (rec.send(attr).length == 0) or (re =~ rec.send(attr))
+    unless !rec.send(attr) || (rec.send(attr).length == 0) || (re =~ rec.send(attr))
       rec.errors.add attr, "looks awry. A URL should look something like http://som/ewh/ere."
     end
   end
-			
-  
 end
